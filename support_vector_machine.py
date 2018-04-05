3a56bf144fa9359e190197880692cf4f2cc0cc05
ryankusch <rkusch@hmc.edu>
2018-04-04 22:30:51 -0700

    Modified svm to print train/test score

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -19,8 +19,13 @@ data = load_data(filenameX, filenamey, header=1)
 
 X, y = data.X, data.y
 
+n_splits = 5
+kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=None)
 
-kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=None)
+svc_train_score = 0.0
+svc_test_score = 0.0
+dummy_train_score = 0.0
+dummy_test_score = 0.0
 
 for train, test in kf.split(X, y):
     X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
@@ -31,5 +36,13 @@ for train, test in kf.split(X, y):
     clf = SVC()
     clf.fit(X_train, y_train)
 
-    print "SVC: %.6f" % (clf.score(X_test, y_test))
-    print "Dummy: %.6f" % (dumclf.score(X_test, y_test))
+    svc_train_score += clf.score(X_train, y_train)
+    svc_test_score += clf.score(X_test, y_test)
+    dummy_train_score += dumclf.score(X_train, y_train)
+    dummy_test_score += dumclf.score(X_test, y_test)
+
+
+print "SVC train: %.6f" % (svc_train_score/n_splits)
+print "Dummy train: %.6f" % (dummy_train_score/n_splits)    
+print "SVC test: %.6f" % (svc_test_score/n_splits)
+print "Dummy test: %.6f" % (dummy_test_score/n_splits)
