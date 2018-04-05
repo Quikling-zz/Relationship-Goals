cff5ac52f48e24c7ce268c7495e9663e2d645175
ryankusch <rkusch@hmc.edu>
2018-04-04 23:11:55 -0700

    Changed Random Forest file to print test/train scores

--- randomforest.py
+++ randomforest.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 # scikit-learn libraries
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
@@ -39,13 +40,18 @@ def bagging_ensemble(X_train, y_train, X_test, y_test, max_features=None, num_cl
     
     Returns
     --------------------
-        accuracy     -- float, accuracy of bagging ensemble classifier on test data
+        #accuracy     -- float, accuracy of bagging ensemble classifier on test data
+        returns a bagging classifier (the above return value corresponds to the three commented
+        out lines below)
     """
     base_clf = DecisionTreeClassifier(criterion='entropy', max_features=max_features)
     clf = BaggingClassifier(base_clf, n_estimators=num_clf)
+    return clf
+    """
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     return metrics.accuracy_score(y_test, y_pred)
+    """
 
 
 def random_forest(X_train, y_train, X_test, y_test, max_features, num_clf=10,
@@ -142,6 +148,10 @@ def plot_histograms(bagging_scores, random_forest_scores):
 def main():
     np.random.seed(1234)
     
+    # below is code from hw7 that may be useful in the future
+    # so it's commented out for now
+
+    """
     # load dataset
     data = utils.load_data("X.csv", "y.csv", header=1)
     X = data.X
@@ -175,12 +185,10 @@ def main():
     random_forest_results = []        
     for m in max_features_vector :
         bagging_results.append(np.median(np.array(bagging_scores)))
-#        print m, np.median(np.array(random_forest_scores[m]))
         random_forest_results.append(np.median(np.array(random_forest_scores[m])))
     plot_scores(max_features_vector, bagging_results, random_forest_results)
     
-    #========================================
-    # plot histograms of performances for max_features=8
+    
     bagging_scores = []
     random_forest_scores = []
     for i in range(num_trials) :
@@ -189,11 +197,34 @@ def main():
         random_forest_scores.append(random_forest(X_train, y_train, X_test, y_test, 8,
                                     bagging=bagging))
     plot_histograms(bagging_scores, random_forest_scores)
-    
-    ### ========== TODO : START ========== ###
-    # part d: determine pixel importance
-    
-    ### ========== TODO : END ========== ###
+    """
+
+    filenameX = 'X.csv'
+    filenamey = 'y.csv'
+
+    data = utils.load_data(filenameX, filenamey, header=1)
+
+    X, y = data.X, data.y
+
+    n_splits = 5
+    kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=None)
+
+    rf_train_score = 0.0
+    rf_test_score = 0.0
+
+    for train_index, test_index in kf.split(X, y):
+        X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
+
+        clf = random_forest(X_train, y_train, X_test, y_test, 8, bagging=bagging_ensemble)
+        clf.fit(X_train, y_train)
+
+        rf_train_score += clf.score(X_train, y_train)
+        rf_test_score += clf.score(X_test, y_test)
+
+
+    print "RF train: %.6f" % (rf_train_score/n_splits)
+    print "RF test: %.6f" % (rf_test_score/n_splits)
+
 
 
 if __name__ == "__main__" :
